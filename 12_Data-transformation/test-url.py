5e42347bd0246f636095399887b63162c74345da
KYourSelfer <kyourselfer@gmail.com>
2022-09-29 20:39:21 +0300

    Create verified request.

---
+++ 12_Data-transformation/test-url.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+
+number=1
+method="GET"
+redirect="True"
+timeout=3
+
+@pytest.mark.parametrize("name,schema,url,port", [
+    ("Google", "https://", "google.com", "443"),
+    ("open-notify.org", "http://", "api.open-notify.org/iss-now.json", "80"),
+])
+
+def test_access(host, name, schema, url, port):
+    testedaddress = host.addr(url)
+    testedaddress.is_resolvable
+    testedaddress.port(port).is_reachable
+#    response = requests.get("http://url",port,params={'method': method, 'redirect': redirect, 'timeout': timeout})
+    response = requests.get(schema+url,port)
+    assert response.status_code == 200
